57a1491002fc6f3d88b87a5efb5eacd020d2737d
nurmasarilarasajii <72746534+nurmasarilarasajii@users.noreply.github.com>
2022-03-30 22:06:34 +0700

    Add files via upload

---
+++ nomer 1.py
@@ -0,0 +1,17 @@
+# Nama  : Nurma Sari Laras Aji
+# NIM   : 20051397062
+# Kelas : MI2020B
+
+class Stack(list):
+    push = list.append
+
+def modify_stack(symbolString):
+    stack=Stack()
+    result = []
+    for character in symbolString:
+        if character != '*':
+            stack.push(character)
+        else:
+            result.append(stack.pop())
+    return ''.join(result)
+print(modify_stack('EAS*Y*QUE***ST***IO*N***'))
